08e6a8322ea4746a72d0a8c3ae03510fc6500eda
Ferdaousse24 <elbaraka23ferdaousse@gmail.com>
2024-06-07 11:33:31 +0200

    ajouter le formulaire de passagers à la page d'accueil

---
+++ src/app/formulaire-reservation/formulaire-reservation.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulaire-reservation',
+  templateUrl: './formulaire-reservation.component.html',
+  styleUrls: ['./formulaire-reservation.component.css'],
+  standalone: true
+})
+export class FormulaireReservationComponent {}
