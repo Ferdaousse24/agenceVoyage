dc68997b9de594d4e6684dd23f6d653613d3a3aa
masterDevelopDev <master.develop.dev@gmail.com>
2024-08-11 19:21:24 +0200

    Update environement.ts

--- src/environement/environement.ts
+++ src/environement/environement.ts
@@ -1,8 +1,8 @@
 export const environement = {
     production: false,
     amadeus: {
-      clientId: 'bRgtP5A7HZfkQ56FIFIeRBuf8UTMV2GO',    //'sK6vbONPyYecAZ8ssqpaqAopgn2RfHwB',
-      clientSecret: 'uJVdgRWxCnGGTBxt'                                   //'WdickTNJYFmuGjtX'
+      clientId: 'sK6vbONPyYecAZ8ssqpaqAopgn2RfHwB',    //'sK6vbONPyYecAZ8ssqpaqAopgn2RfHwB', 'bRgtP5A7HZfkQ56FIFIeRBuf8UTMV2GO'
+      clientSecret: 'WdickTNJYFmuGjtX'                                   //'WdickTNJYFmuGjtX',   'uJVdgRWxCnGGTBxt'
     }
   };
   
